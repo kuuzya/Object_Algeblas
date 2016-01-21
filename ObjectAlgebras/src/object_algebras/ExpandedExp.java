4642e0513f1aede61bdf0e5d0bc3d9a58dfb8772
kuuzya <sora_dice@yahoo.co.jp>
2016-01-21 13:05:43 +0900

    Add ExpandedFactory and ExpandedIntPrint

--- ObjectAlgebras/src/object_algebras/ExpandedExp.java
+++ ObjectAlgebras/src/object_algebras/ExpandedExp.java
@@ -27,3 +27,59 @@ class Div implements Exp{
 	}
 }
 
+interface ExpandedIntAlg<A> extends IntAlg<A> {
+	A minus(A e1, A e2);
+	A multi(A e1, A e2);
+	A div(A e1, A e2);
+}
+
+class ExpandedIntFactory extends IntFactory implements ExpandedIntAlg<Exp> {
+	@Override
+	public Exp minus(Exp e1, Exp e2) {
+		return new Minus(e1, e2);
+	}
+
+	@Override
+	public Exp multi(Exp e1, Exp e2) {
+		return new Multi(e1, e2);
+	}
+
+	@Override
+	public Exp div(Exp e1, Exp e2) {
+		return new Div(e1, e2);
+	}
+
+}
+
+class ExpandedIntPrint extends IntPrint implements ExpandedIntAlg<IPrint>{
+	@Override
+	public IPrint minus(IPrint e1, IPrint e2) {
+		return new IPrint(){
+			@Override
+			public String print() {
+				return e1.print() + "-" + e2.print();
+			}
+		};
+	}
+
+	@Override
+	public IPrint multi(IPrint e1, IPrint e2) {
+		return new IPrint(){
+			@Override
+			public String print() {
+				return e1.print() + "*" + e2.print();
+			}
+		};
+	}
+
+	@Override
+	public IPrint div(IPrint e1, IPrint e2) {
+		return new IPrint(){
+			@Override
+			public String print() {
+				return e1.print() + "/" + e2.print();
+			}
+		};
+	}
+
+}
