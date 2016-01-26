8d0053ef7e1743a5a99630304c6d79f7bdd74560
kuuzya <sora_dice@yahoo.co.jp>
2016-01-26 13:09:15 +0900

    Change multi and div method in fouropereationStringFactory
    return value is changed from e1+e2 to (e1+e2)

--- ObjectAlgebras/src/fouroperation/FourOperationStringFactory.java
+++ ObjectAlgebras/src/fouroperation/FourOperationStringFactory.java
@@ -4,5 +4,14 @@ import addsub.AddSubStringFactory;
 import multidiv.MultiDivStringFactory;
 
 public interface FourOperationStringFactory extends AddSubStringFactory, MultiDivStringFactory, FourOperationAlg<String> {
+	@Override
+	public default String add(String e1, String e2) {
+		return "(" + e1 + " + " + e2 + ")";
+	}
+
+	@Override
+	public default String sub(String e1, String e2) {
+		return "(" + e1 + " - " + e2 + ")";
+		}
 
 }
